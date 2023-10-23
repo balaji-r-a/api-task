5adffa2298d29699e8932e751d069327a878460e
balaji r a <145047750+balaji-r-a@users.noreply.github.com>
2023-10-23 23:27:14 +0400

    Add files via upload
    first imp

---
+++ api.js
@@ -0,0 +1,21 @@
+const uname = document.querySelector("#name");
+const email = document.querySelector("#email");
+const comment = document.querySelector("#comment");
+const row = document.querySelector(".row");
+const showCard = document.querySelector("#showCard");
+
+function apiFetch() {
+  fetch("https://jsonplaceholder.typicode.com/comments")
+    .then((res) => res.json())
+    .then((allDatas) => displayData(allDatas));
+}
+apiFetch();
+
+function displayData(data) {
+  data.forEach((e) => {
+    uname.innerHTML = e.name;
+    email.innerHTML = e.email;
+    comment.innerHTML = e.body;
+    row.innerHTML += showCard.innerHTML;
+  });
+}
